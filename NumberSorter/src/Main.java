5edd71c8a4feaa2f5ecf325f88b7bf3ea8e8c7b8
Özgür Işık <ozgur.isk@icloud.com>
2024-02-23 14:40:07 +0300

    First version of sorter

---
+++ NumberSorter/src/Main.java
@@ -0,0 +1,43 @@
+import sun.awt.AWTAccessor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter number 'a': ");
+        a = inp.nextInt();
+
+        System.out.print("Enter number 'b': ");
+        b = inp.nextInt();
+
+        System.out.print("Enter number 'c': ");
+        c = inp.nextInt();
+
+        if (a < b && a < c) {
+            if (b < c) {
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
+            }
+        } else if(b < a && b < c){
+            if(a < c){
+                System.out.println("b < a < c");
+            }else{
+                System.out.println("b < c < a");
+            }
+
+        }else{
+            if(a < b){
+                System.out.println("c < a < b");
+            }else{
+                System.out.println("c < b < a");
+            }
+        }
+
+
+    }
+}
